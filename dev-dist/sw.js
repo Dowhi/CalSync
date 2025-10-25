fb501bf3186782c4c25b8b633b8e901f57c66a66
Tu Nombre <tu.email@ejemplo.com>
2025-10-25 20:18:17 +0200

    Fix: Cambiado height del calendario de 100% a auto para que se renderice correctamente

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.eq5qs2usob8"
+    "revision": "0.u9jamh2ndio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
